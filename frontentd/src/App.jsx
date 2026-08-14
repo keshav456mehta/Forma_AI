b736e8a910c187c3b23a4fd674196d2c287ae8d9
Rakesh <mouryarakesh512@gmail.com>
2026-08-14 22:10:32 +0530

    day 6 and day 7 done

---
+++ frontentd/src/App.jsx
@@ -0,0 +1,11 @@
+import DynamicForm from "./components/DynamicForm";
+
+function App() {
+  const formId = "YOUR_FORM_ID";
+
+  return (
+    <DynamicForm formId={formId} />
+  );
+}
+
+export default App;
